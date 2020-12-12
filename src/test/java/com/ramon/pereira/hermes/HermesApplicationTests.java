5292589b8e65405d0f03663b69560018af891ff6
Ramon Pereira <apereira.ramon@gmail.com>
2020-12-12 14:45:14 -0300

    configure tests and test application yml envs

--- src/test/java/com/ramon/pereira/hermes/HermesApplicationTests.java
+++ src/test/java/com/ramon/pereira/hermes/HermesApplicationTests.java
@@ -1,13 +1,15 @@
 package com.ramon.pereira.hermes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class HermesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void main() {
+		HermesApplication.main(new String[] {"--spring.config.location=classpath:application-test.yml"});
 	}
 
 }
