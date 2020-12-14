583926807bc58181fb7b1310431183a507212558
Ramon Pereira <56852446+ramoonpereira@users.noreply.github.com>
2020-12-14 11:20:09 -0300

    update app to set timezone default

--- src/main/java/com/ramon/pereira/hermes/HermesApplication.java
+++ src/main/java/com/ramon/pereira/hermes/HermesApplication.java
@@ -2,11 +2,13 @@ package com.ramon.pereira.hermes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class HermesApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(HermesApplication.class, args);
 	}
 
